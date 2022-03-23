d9493323cfd876dfa23c0033e7e8342a8d04b014
Siergus <sirotto@gmail.com>
2022-03-23 21:45:23 +0100

    Added logic and performed anothere cleanup

---
+++ src/components/LikeButton/LikeButton.jsx
@@ -0,0 +1,12 @@
+import CustomizedButton from "../CustomizedButton/CustomizedButton";
+
+const LikeButton = ({onClick}) => {
+    return <CustomizedButton
+        className="button like-button"
+        aria-label="do-like"
+        onClick={onClick}
+        name="Like">
+    </CustomizedButton>;
+}
+
+export default LikeButton;
